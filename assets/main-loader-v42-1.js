26c75eb72b76aaf937b27d0f8eb0e50ca5d0ce20
amirrezasoleimani007 <amirreza.soleimani.n@gmail.com>
2026-09-11 08:59:15 +0330

    Publish uncached IPS dashboard v42.1

---
+++ assets/main-loader-v42-1.js
@@ -0,0 +1,9 @@
+const partNames=["index-v42-1.part-00","index-v42-1.part-01","index-v42-1.part-02","index-v42-1.part-03","index-v42-1.part-04"];
+const baseUrl=new URL("./",import.meta.url);
+const sourceParts=await Promise.all(partNames.map(async(name)=>{
+  const response=await fetch(new URL(name,baseUrl));
+  if(!response.ok)throw new Error(`Unable to load dashboard bundle part: ${name}`);
+  return response.text();
+}));
+const moduleUrl=URL.createObjectURL(new Blob([sourceParts.join("")],{type:"text/javascript"}));
+try{await import(moduleUrl);}finally{setTimeout(()=>URL.revokeObjectURL(moduleUrl),0);}
